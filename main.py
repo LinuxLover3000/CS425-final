d6fb431e9917736a21c24699c62555f33f286f0f
gmendoza4 <gmendoza4@hawk.iit.edu>
2023-08-03 03:44:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def display_results(columns, data):
     tree = ttk.Treeview(results, columns=columns, show='headings')
     tree.pack()
 
-    # Set column headings
+    
     for col in columns:
         tree.heading(col, text=col)
-        tree.column(col, width=100)  # Adjust width as needed
+        tree.column(col, width=100)  
 
     # Insert data into the treeview
     for row in data:
@@ -56,11 +56,11 @@ def admin_mode():
 
 def employee_mode():
     clear_options()
-    show_buttons(patient_nurse_queries)
+    show_buttons(employee_queries)
 
 def patient_mode():
     clear_options()
-    show_buttons(doctor_nurse_queries)
+    show_buttons(patient_queries)
 
 def clear_options():
     for widget in options_frame.winfo_children():
@@ -107,18 +107,18 @@ options_frame.pack(pady=20)
 # Queries for different user roles
 queries = [
     ("select * from patient inner join patient_phone on patient.mrn=patient_phone.mrn", "Join patient + patient's phone number"),
-                ("update patient_phone set phone=\"7735068524\" where mrn=\"1487426\"", "Update a phone number"),
-                ("select * from (select name_first, name_last, bill.MRN, issue_date, amount, rank() over (order by amount desc) as \"Priority by Quantity\" from (patient inner join bill on patient.mrn = bill.mrn)) as table1", "Sort bill quantities in the hospital by quantity"),
-                ("select * from (select name_first, name_last, bill.MRN, issue_date, amount, rank() over (order by due_date) as \"Priority by Issue Date\" from (patient inner join bill on patient.mrn = bill.mrn)) as table2", "Sort bill with dated payments priority"),
-                ("select *, date_format(from_days(datediff(now(), DOB)), \"%Y\") + ntile(3) over(order by DOB desc) as \"age range\" from patient", "Find the age distribution in the patients"),
-                ("select name_first, name_last, type, round(round(cume_dist() over(order by type), 2) * 100, 2) as \"Specialty Distribution(%)\" from doctor as Table1", "Group doctors by specialty and distribution"),
-                ("Select * from nurse where EID = (SELECT EID FROM room GROUP BY EID HAVING COUNT(*) = 1)", "Get the info on nurses assigned to only one room"),
-                ("Select MRN, patient.name_first, patient.name_last from patient, doctor Where patient.EID = doctor.EID and doctor.name_last =\'Barry\'", "Get the MRN and name from patients with Dr. Barry"),
-                ("SELECT * FROM employee WHERE EXISTS (SELECT * FROM nurse WHERE nurse.EID = employee.EID)", "List all the employees that are nurses"),
-                ("SELECT nurse.* FROM nurse INNER JOIN room ON nurse.EID = room.EID WHERE room.surgery_capable = 1;", "List all the nurses assigned to operating rooms"),
-                ("SELECT * from nurse where type = \"Anesthetist\"", "get the info on nurses that are Anesthetists"),
-                ("SELECT type, FORMAT(AVG(salary), 2) as average_salary FROM doctor GROUP BY type", "find the average salaries of each type of doctor"),
-                ("""SELECT nurse.type as employee_type, employee_phone.EID, nurse.name_first,
+    ("update patient_phone set phone=\"7735068524\" where mrn=\"1487426\"", "Update a phone number"),
+    ("select * from (select name_first, name_last, bill.MRN, issue_date, amount, rank() over (order by amount desc) as \"Priority by Quantity\" from (patient inner join bill on patient.mrn = bill.mrn)) as table1", "Sort bill quantities in the hospital by quantity"),
+    ("select * from (select name_first, name_last, bill.MRN, issue_date, amount, rank() over (order by due_date) as \"Priority by Issue Date\" from (patient inner join bill on patient.mrn = bill.mrn)) as table2", "Sort bill with dated payments priority"),
+    ("select *, date_format(from_days(datediff(now(), DOB)), \"%Y\") + ntile(3) over(order by DOB desc) as \"age range\" from patient", "Find the age distribution in the patients"),
+    ("select name_first, name_last, type, round(round(cume_dist() over(order by type), 2) * 100, 2) as \"Specialty Distribution(%)\" from doctor as Table1", "Group doctors by specialty and distribution"),
+    ("Select * from nurse where EID = (SELECT EID FROM room GROUP BY EID HAVING COUNT(*) = 1)", "Get the info on nurses assigned to only one room"),
+    ("Select MRN, patient.name_first, patient.name_last from patient, doctor Where patient.EID = doctor.EID and doctor.name_last =\'Barry\'", "Get the MRN and name from patients with Dr. Barry"),
+    ("SELECT * FROM employee WHERE EXISTS (SELECT * FROM nurse WHERE nurse.EID = employee.EID)", "List all the employees that are nurses"),
+    ("SELECT nurse.* FROM nurse INNER JOIN room ON nurse.EID = room.EID WHERE room.surgery_capable = 1;", "List all the nurses assigned to operating rooms"),
+    ("SELECT * from nurse where type = \"Anesthetist\"", "get the info on nurses that are Anesthetists"),
+    ("SELECT type, FORMAT(AVG(salary), 2) as average_salary FROM doctor GROUP BY type", "find the average salaries of each type of doctor"),
+    ("""SELECT nurse.type as employee_type, employee_phone.EID, nurse.name_first,
                     nurse.name_last, employee_phone.phone
                     FROM employee_phone
                     LEFT JOIN nurse ON employee_phone.EID = nurse.EID
@@ -130,28 +130,31 @@ queries = [
                     LEFT JOIN doctor ON employee_phone.EID = doctor.EID
                     WHERE doctor.type = 'Surgeon'
                     Order by employee_type""", "list the phone numbers of employees that may be working in the operating room (Surgical and Anesthetist nurses and Surgeon doctors) and order it by employee type"),
-                ("select *, TIMESTAMPDIFF(YEAR, DOB, CURDATE()) as \"age\" from patient Where TIMESTAMPDIFF(YEAR, DOB, CURDATE()) > 30", "Get info on all patients older than 30"),
-                ("select amount from bill where datediff(due_date, curdate()) < 0", "Get amounts of late bills"),
-                ("""Select employee.name_first, employee.name_last from employee,
+    ("select *, TIMESTAMPDIFF(YEAR, DOB, CURDATE()) as \"age\" from patient Where TIMESTAMPDIFF(YEAR, DOB, CURDATE()) > 30", "Get info on all patients older than 30"),
+    ("select amount from bill where datediff(due_date, curdate()) < 0", "Get amounts of late bills"),
+    ("""Select employee.name_first, employee.name_last from employee,
                     (select EID from employee_phone group by EID having count(EID) > 1) as subquery
                     where employee.EID = subquery.EID;""", "Get names of employees with more than one phone number"),
-                ("""Select doctor.name_first, doctor.name_last from doctor,
+    ("""Select doctor.name_first, doctor.name_last from doctor,
                     inner join(
                     (select * from report where timestampdiff(date, curdate()) <= 31)) as subquery
                     where doctor.EID = subquery.EID""", "Get doctors who have written at least one report in the past month"),
-                ("select *, ntile(4) over (order by salary) from employee order by salary", "Separate employees into salary quartiles")
+    ("select *, ntile(4) over (order by salary) from employee order by salary", "Separate employees into salary quartiles")
 ]
 
-patient_nurse_queries = [
-    ("patient_query_1", "Patient Query 1"),
-    ("nurse_query_1", "Nurse Query 1"),
-    # ... (more patient and nurse queries)
-]
-
-doctor_nurse_queries = [
-    ("doctor_query_1", "Doctor Query 1"),
+employee_queries = [
+    ("SELECT CONCAT(nurse.name_last, ', ', nurse.name_first) AS name, room.number as room_assignment, patient.MRN AS patient_assignment FROM nurse INNER JOIN room ON nurse.EID = room.EID LEFT JOIN patient ON room.MRN = patient.MRN order by name", "Nurse Assignments"),
+    ("SELECT CONCAT(doctor.name_last, ', ', doctor.name_first) AS name, room.number as room_assignment, patient.MRN AS patient_assignment FROM doctor, patient, room Where patient.EID = doctor.EID and patient.MRN = room.MRN order by name", "Doctor Assignments"),
+    ("SELECT doctor.type AS department, CONCAT(employee.name_last, ', ', employee.name_first) AS name, employee_phone.phone as phone_number FROM employee, employee_phone, doctor, nurse Where employee.EID = employee_phone.EID and doctor.EID = employee.EID UNION SELECT doctor.type AS department, CONCAT(employee.name_last, ', ', employee.name_first) AS name, employee_phone.phone as phone_number FROM employee, employee_phone, doctor, nurse Where employee.EID = employee_phone.EID and nurse.EID = employee.EID ORDER BY department, name", "Employee Contact List"),
     ("nurse_query_1", "Nurse Query 1"),
     # ... (more doctor and nurse queries)
+]
+
+patient_queries = [
+    ("select doctor.type as department, CONCAT(doctor.name_first, ' ', doctor.name_last) AS name, employee_phone.phone from doctor inner join employee_phone on doctor.EID=employee_phone.EID order by type", "Doctor Contact info"),
+    ("select nurse.type as department, CONCAT(nurse.name_first, ' ', nurse.name_last) AS name, employee_phone.phone from nurse inner join employee_phone on nurse.EID=employee_phone.EID order by type", "Nurse Contact info"),
+    ("patient_query_1", "patient Query 1"),
+    # ... (more patient queries)
 ]
 
 buttons = Frame(window)
